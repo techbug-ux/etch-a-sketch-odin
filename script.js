9ae72ee4902c23e7ff2f03b4783244fa929e7804
Bob Parker <bob@spotme.com>
2026-06-03 12:13:38 -0400

    Added the hovering effect on the sketch

--- script.js
+++ script.js
@@ -9,4 +9,22 @@ console.log("hello world")
 for (let i = 0; i < 269; i++) {
     const square = document.createElement("div");
     document.body.appendChild(square);
+    
 }
+
+let rome = document.getElementsByClassName("container");
+
+
+
+document.body.addEventListener('mouseover', e => {
+  const div = e.target.closest('div');
+  if (!div) return;
+  div.style.backgroundColor = 'black';
+});
+
+document.body.addEventListener('mouseout', e => {
+  const div = e.target.closest('div');
+  if (!div) return;
+  div.style.backgroundColor = 'black';
+});
+
